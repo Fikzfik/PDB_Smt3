2c880f12cce628de779511c7cf7800120a843326
Fikri Ardiansyah <137857081+Fikzfik@users.noreply.github.com>
2024-11-19 09:37:05 +0700

    Menambahkan beberapa view procedure saat presentasi

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -7,6 +7,8 @@ use Illuminate\View\View;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\DetailPengadaan;
+use App\Models\ViewPengadaan;
 
 class ViewController extends Controller
 {
@@ -15,15 +17,7 @@ class ViewController extends Controller
         $validUser = Auth::user();
 
         // Join pengadaan, detail_pengadaan, barang, satuan, vendor, and users
-        $detail = DB::select('SELECT p.idpengadaan, u.username, v.nama_vendor, p.subtotal_nilai, p.total_nilai, p.ppn, p.status, p.timestamp,
-            dp.iddetail_pengadaan, b.nama, dp.harga_satuan, dp.jumlah, dp.sub_total, s.nama_satuan
-            FROM pengadaan p
-            JOIN users u ON p.users_iduser = u.iduser
-            JOIN vendor v ON p.vendor_idvendor = v.idvendor
-            JOIN detail_pengadaan dp ON p.idpengadaan = dp.idpengadaan
-            JOIN barang b ON dp.idbarang = b.idbarang
-            JOIN satuan s ON b.idsatuan = s.idsatuan
-    ');
+        $detail = DetailPengadaan::all();
         $pengadaans = DB::select('SELECT p.idpengadaan, u.username, v.nama_vendor, p.subtotal_nilai, p.total_nilai, p.ppn, p.status, p.timestamp
             FROM pengadaan p
             JOIN users u ON p.users_iduser = u.iduser
@@ -47,11 +41,7 @@ class ViewController extends Controller
     public function dashboarduser()
     {
         $validUser = Auth::user();
-        $pengadaans = DB::select('SELECT p.idpengadaan, u.username, v.nama_vendor, p.subtotal_nilai, p.total_nilai, p.ppn, p.status
-            FROM pengadaan p
-            JOIN users u ON p.users_iduser = u.iduser
-            JOIN vendor v ON p.vendor_idvendor = v.idvendor
-        ');
+        $pengadaans = ViewPengadaan::all();
         // @dd($pengadaans);
         return view('dashboarduser', compact('validUser', 'pengadaans'));
     }
